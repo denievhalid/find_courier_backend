b47a8662cb0a1af70cb545256b29ef4e7c254fac
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-08-05 17:52:41 +0300

    fix

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ import { ENV } from "./constants";
 
 async function createDatabaseConnection(): Promise<Mongoose> {
   //return mongoose.connect(getEnvProperty(ENV.MONGOOSE_URI) as string);
-  return mongoose.connect("mongodb://localhost:27017/courier");
+  return mongoose.connect("mongodb://127.0.0.1:27017/courier");
 }
 
 export { createDatabaseConnection };
