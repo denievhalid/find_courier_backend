aa09da659f3121101cca31d5f15ca72adf5a9d96
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-08-08 02:22:58 +0300

    commit

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ function useMiddlewares(server: Express) {
 
 function useRoutes(server: Express) {
   server.get("/", (req, res) => {
-    res.send("hello 2 55");
+    res.send("123");
   });
   server.use(ROUTES.ORDER, orderRoutes);
 }
