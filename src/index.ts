aa4ea7fa510d0ece43d3c05a476c1a3eb226a0e2
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-08-09 03:42:33 +0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import { createServer } from "./server";
 import { createDatabaseConnection } from "./database";
 
 createDatabaseConnection().then(createServer);
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
