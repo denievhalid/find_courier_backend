b6065d4f21fb71df65ad4931c530a52bb08277a1
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-08-18 13:41:51 +0300

    commit

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,9 @@
 import { I18n } from "i18n";
-import path from "path";
 
 const i18n = new I18n({
   locales: ["en", "ru"],
   defaultLocale: "ru",
-  directory: path.join(__dirname, "locales"),
+  directory: "locales",
 });
 
 export default i18n;
