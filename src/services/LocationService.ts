df2b57a3f16f550c042ef7eb2ea5f5ca230566dc
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-09-03 02:57:52 +0300

    commit

--- src/services/LocationService.ts
+++ src/services/LocationService.ts
@@ -1,10 +1,10 @@
-import { LocationType } from "../types";
-//import LocationModel from "../models/LocationModel";
-
-class LocationService<T> {
-  getByKladr(city_kladr_id: number) {
-    //return LocationModel.findOne({ city_kladr_id });
-  }
-}
-
-export default new LocationService<LocationType>();
+// import { LocationType } from "../types";
+// import LocationModel from "../models/LocationModel";
+//
+// class LocationService<T> {
+//   getByKladr(city_kladr_id: number) {
+//     return LocationModel.findOne({ city_kladr_id });
+//   }
+// }
+//
+// export default new LocationService<LocationType>();
