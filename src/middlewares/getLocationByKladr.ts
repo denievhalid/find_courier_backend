ee61942e44fe1ede177a688e50d5059c080cb685
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-09-03 02:58:13 +0300

    commit

--- src/middlewares/getLocationByKladr.ts
+++ src/middlewares/getLocationByKladr.ts
@@ -1,22 +1,22 @@
-import _ from "lodash";
-import type { NextFunction, Response, Request } from "express";
-import LocationService from "../services/LocationService";
-
-export async function getLocationByKladr(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  return next();
-  const { from, to } = req.query;
-
-  try {
-    _.forEach([from, to], (el) => {});
-    const item = await LocationService.getByKladr(Number(req.query?.from));
-    // @ts-ignore
-    req.query.from = item;
-    next();
-  } catch (err) {
-    next(err);
-  }
-}
+// import _ from "lodash";
+// import type { NextFunction, Response, Request } from "express";
+// import LocationService from "../services/LocationService";
+//
+// export async function getLocationByKladr(
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) {
+//   return next();
+//   const { from, to } = req.query;
+//
+//   try {
+//     _.forEach([from, to], (el) => {});
+//     const item = await LocationService.getByKladr(Number(req.query?.from));
+//     // @ts-ignore
+//     req.query.from = item;
+//     next();
+//   } catch (err) {
+//     next(err);
+//   }
+// }
