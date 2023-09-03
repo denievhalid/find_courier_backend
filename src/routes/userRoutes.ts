3e5bd75452539b508594f66a1e05244ffcde510e
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-09-04 02:56:16 +0300

    commit

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,25 +2,4 @@ import UserController from "../controllers/UserController";
 import { configureRoutes } from "../utils/configureRoutes";
 import checkAuth from "../middlewares/checkAuth";
 
-export default configureRoutes([
-  {
-    path: "/login",
-    method: "post",
-    actions: [UserController.login],
-  },
-  {
-    path: "/verify",
-    method: "post",
-    actions: [UserController.verify],
-  },
-  {
-    path: "/:id",
-    method: "post",
-    actions: [UserController.register],
-  },
-  {
-    path: "/",
-    method: "patch",
-    actions: [checkAuth, UserController.update],
-  },
-]);
+export default configureRoutes([]);
