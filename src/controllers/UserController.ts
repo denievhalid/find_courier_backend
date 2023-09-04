fb3ca0c9cd17ef7fb0ad9d95e30d50569ea4d4ec
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-09-04 08:12:11 +0300

    commit

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,6 +6,15 @@ import getEnvProperty from "../utils/getEnvProperty";
 import { ENV } from "../constants";
 import type { RequestWithUserType } from "../types";
 
-class UserController {}
+class UserController {
+  async changeAvatar(req: Request, res: Response) {
+    try {
+      console.log(req.files);
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      return res.status(500).json({ error, success: false });
+    }
+  }
+}
 
 export default new UserController();
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,12 @@
 import UserController from "../controllers/UserController";
 import { configureRoutes } from "../utils/configureRoutes";
 import checkAuth from "../middlewares/checkAuth";
+import multer from "../utils/multer";
 
-export default configureRoutes([]);
+export default configureRoutes([
+  {
+    path: "/changeAvatar",
+    method: "post",
+    actions: [multer.array("avatar", 1), UserController.changeAvatar],
+  },
+]);
