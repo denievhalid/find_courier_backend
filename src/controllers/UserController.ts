0327019ad8c6e4cd1dc9fdab4cf1efeeb0d42804
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-09-05 06:10:13 +0300

    commit

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import UserService from "../services/UserService";
+import _ from "lodash";
 import jwt from "jsonwebtoken";
 import i18n from "../i18n";
 import getEnvProperty from "../utils/getEnvProperty";
@@ -8,11 +9,16 @@ import type { RequestWithUserType } from "../types";
 
 class UserController {
   async create(req: Request, res: Response) {
-    const {
+    let {
       body: { avatar, name, gender },
     } = req;
 
     try {
+      if (_.isObject(gender)) {
+        // @ts-ignore
+        gender = gender?.id;
+      }
+
       await UserService.create({ avatar, name, gender });
       return res.status(201).json({ success: true });
     } catch (error) {
