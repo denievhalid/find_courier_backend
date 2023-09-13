95cf35bd455bc07e601ce5507f50718f4799fe37
Дениев Халид Джамалайлаевич <hdeniev@rtk-element.ru>
2023-09-14 01:40:01 +0300

    commit

--- src/services/DeliveryService.ts
+++ src/services/DeliveryService.ts
@@ -2,7 +2,7 @@ import DeliveryModel from "../models/DeliveryModel";
 
 class DeliveryService {
   getByUserId(userId: string) {
-    return DeliveryModel.find({ user: userId });
+    return DeliveryModel.find({ user: userId }).populate("user");
   }
 }
 
